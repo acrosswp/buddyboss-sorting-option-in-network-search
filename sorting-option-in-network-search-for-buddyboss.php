19b1faf0d927fbc18794a2a067dede52870de975
raftaar1191 <raftaar1191@gmail.com>
2023-05-06 19:30:07 +0530

    Remove unwanted file

--- sorting-option-in-network-search-for-buddyboss.php
+++ sorting-option-in-network-search-for-buddyboss.php
@@ -37,26 +37,6 @@ if ( ! defined( 'WPINC' ) ) {
  */
 define( 'SORTING_OPTION_IN_NETWORK_SEARCH_FOR_BUDDYBOSS_FILES', __FILE__ );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-sorting-option-in-network-search-for-buddyboss-activator.php
- */
-function activate_sorting_option_in_network_search_for_buddyboss() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sorting-option-in-network-search-for-buddyboss-activator.php';
-	Sorting_Option_In_Network_Search_For_BuddyBoss_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-sorting-option-in-network-search-for-buddyboss-deactivator.php
- */
-function deactivate_sorting_option_in_network_search_for_buddyboss() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sorting-option-in-network-search-for-buddyboss-deactivator.php';
-	Sorting_Option_In_Network_Search_For_BuddyBoss_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_sorting_option_in_network_search_for_buddyboss' );
-register_deactivation_hook( __FILE__, 'deactivate_sorting_option_in_network_search_for_buddyboss' );
 
 /**
  * The core plugin class that is used to define internationalization,
